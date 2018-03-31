70894fa180151c8df3b282c8adf7f84ce56cf769
Pierre-François Lucet <pierre-francois@dacker.co>
2018-03-31 18:58:51 +0200

    readme

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
 
 setup(
     name='pyspreadsheet',
-    version='0.0.3',
+    version='0.0.4',
     description='Easily send data to Google Sheets',
     long_description=readme,
     author='Dacker',
