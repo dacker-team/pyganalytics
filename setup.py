2148bbe781aeb2777ffaf24fd2a74fcc42b910f6
Pierre-François Lucet <pierre-francois@dacker.co>
2018-04-16 15:58:42 +0200

    pip.req

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@
 
 from setuptools import setup, find_packages
 
-from pip.req import parse_requirements
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 reqs = parse_requirements("requirements.txt", session='hack')
 reqs = [str(ir.req) for ir in reqs]
