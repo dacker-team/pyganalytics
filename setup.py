125c6b145fafa0cc6185de2c4bd895e4439909c8
Pierre-François Lucet <pierre-francois@dacker.co>
2018-08-22 18:06:18 +0200

    v20

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('LICENSE') as f:
 
 setup(
     name='pyganalytics',
-    version='0.0.19',
+    version='0.0.20',
     description='Easily get data from Google Analytics',
     long_description=readme,
     author='Dacker',
