4671c3f638e9b91c2ff78610f06d612719a20b30
Vincent Monet <vincent@dacker.co>
2018-12-04 15:38:35 +0100

    requirements

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open('LICENSE') as f:
 
 setup(
     name='pyganalytics',
-    version='0.0.23',
+    version='0.0.24',
     description='Easily get data from Google Analytics',
     long_description=readme,
     author='Dacker',
