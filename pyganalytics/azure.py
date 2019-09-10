e23933a5fbcc3c01b5045e2820c21ade71be4fdb
Pierre-François Lucet <pierre-francois@dacker.co>
2019-09-10 21:02:53 +0200

    without pyzure

--- pyganalytics/azure.py
+++ pyganalytics/azure.py
@@ -1,5 +1,5 @@
 import pyodbc
-import pyzure
+# import pyzure
 
 
 def to_azure(data, all_batch_id, azure_instance):
@@ -8,10 +8,10 @@ def to_azure(data, all_batch_id, azure_instance):
     if all_batch_id:
         try:
             query = 'DELETE FROM ' + azure_table + ' WHERE batch_id IN ' + "(" + ",".join(all_batch_id) + ");"
-            pyzure.execute.execute_query(azure_instance, query)
+            # pyzure.execute.execute_query(azure_instance, query)
         except pyodbc.ProgrammingError:
             pass
     data["columns_name"] = [r.replace(":", "_") for r in data["columns_name"]]
-    pyzure.send_to_azure(azure_instance, data, replace=False)
+    # pyzure.send_to_azure(azure_instance, data, replace=False)
     return 0
 
