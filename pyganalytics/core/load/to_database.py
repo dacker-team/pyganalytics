290eb84562fe039348c8f47a2cfce1bfa4700c1b
Pierre-François Lucet <pierre-francois@dacker.co>
2019-09-26 16:38:21 +0200

    table does not existr

--- pyganalytics/core/load/to_database.py
+++ pyganalytics/core/load/to_database.py
@@ -7,11 +7,11 @@ def send_to_db(dbstream: DBStream, data, all_batch_id):
     all_batch_id = ["'" + e + "'" for e in all_batch_id]
     table_name = data["table_name"]
     if all_batch_id:
-        # try:
-        query = "DELETE FROM %s WHERE batch_id IN (%s);" % (table_name, ",".join(all_batch_id))
-        dbstream.execute_query(query)
-    # except:
-    #     print("Table does not exist")
+        try:
+            query = "DELETE FROM %s WHERE batch_id IN (%s);" % (table_name, ",".join(all_batch_id))
+            dbstream.execute_query(query)
+        except:
+            print("Table does not exist")
     data["columns_name"] = [r.replace(":", "_") for r in data["columns_name"]]
     copy_data = copy.deepcopy(data)
     total_length = len(copy.deepcopy(data)["rows"])
