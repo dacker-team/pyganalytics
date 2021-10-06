210267640e9388dd0bc90a9f1bfdd6af2f65b70c
geoffreyChet <73818181+geoffreyChet@users.noreply.github.com>
2021-10-06 19:18:16 +0200

    Upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='pyganalytics',
-    version='0.1.27',
+    version='0.1.29',
     description='Easily get data from Google Analytics',
     long_description=readme,
     long_description_content_type="text/markdown",
